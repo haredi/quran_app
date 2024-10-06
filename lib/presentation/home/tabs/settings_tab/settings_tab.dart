4a76d9562fe51d6f6965357d30e2eedadf40fafb
haredi <khaledothman929@gmail.com>
2024-10-06 05:16:20 +0300

    + settings tab

--- lib/presentation/home/tabs/settings_tab/settings_tab.dart
+++ lib/presentation/home/tabs/settings_tab/settings_tab.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quran_app/presentation/home/tabs/settings_tab/widgets/lunguage_bottom_sheet.dart';
+import 'package:quran_app/presentation/home/tabs/settings_tab/widgets/theme_bottom_sheet.dart';
 
 class SettingsTab extends StatelessWidget {
   const SettingsTab({super.key});
@@ -6,7 +8,79 @@ class SettingsTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.grey,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Theme',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            SizedBox(
+              height: 4,
+            ),
+            InkWell(
+              onTap: () {
+                showThemeBottomSheet(context);
+              },
+              child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 4, vertical: 8),
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                          color: Theme.of(context).dividerColor, width: 2),
+                      borderRadius: BorderRadius.circular(12)),
+                  child: Text(
+                    'Light',
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodySmall
+                        ?.copyWith(fontSize: 14),
+                  )),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Language',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            SizedBox(
+              height: 4,
+            ),
+            InkWell(
+              onTap: () {
+                showLanguageBottomSheet(context);
+              },
+              child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 4, vertical: 8),
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                          color: Theme.of(context).dividerColor, width: 2),
+                      borderRadius: BorderRadius.circular(12)),
+                  child: Text(
+                    'English',
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodySmall
+                        ?.copyWith(fontSize: 14),
+                  )),
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  void showThemeBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) => ThemeBottomSheet(),
+    );
+  }
+
+  void showLanguageBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+        context: context, builder: (context) => LanguageBottomSheet());
   }
 }
